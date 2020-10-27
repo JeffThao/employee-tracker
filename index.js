96b236db83b957d06ef2ddbcb3a4dcd0fdaf3fdc
Jeff T <jeff.thao92@gmail.com>
2020-10-27 17:52:45 -0500

    Deleted viewManager and view employees by manager.

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function mainMenu(){
               value: "viewAllRoles"
             },
             {
-                name: "View Departments",
+                name: "View All Departments",
                 value: "viewDept"
               },
             {
@@ -46,20 +46,12 @@ function mainMenu(){
               value: "viewEmployeesByDept"
             },
             {
-              name: "View Employees By Manager",
-              value: "viewEmployeesByManager"
-            },
-            {
-              name: "Add an Employee",
-              value: "addEmployee"
-            },
-            {
               name: "Update Employee Role",
               value: "updateEmployeeRole"
             },
             {
-              name: "Update Employee Manager",
-              value: "updateEmployeeManager"
+              name: "Add an Employee",
+              value: "addEmployee"
             },
             {
               name: "Add a Role",
@@ -99,26 +91,15 @@ function mainMenu(){
               console.log("works")
               viewDepartment()
               break;
-              case "viewEmployeesByManager":
-              // code block
-              console.log("works")
-              // Come back to
-              viewByManager()
-              break;
-              case "addEmployee":
-              // code block
-              console.log("works")
-              addEmployees()
-              break;
               case "updateEmployeeRole":
               // code block
               console.log("works")
               addRoles()
               break;
-              case "updateEmployeeManager":
+              case "addEmployee":
               // code block
               console.log("works")
-              addManager()
+              addEmployees()
               break;
               case "addRole":
               // code block
@@ -130,47 +111,42 @@ function mainMenu(){
               console.log("works")
               addDepartment()
               break;
-              case "quit":
-              // code block
-              console.log("works")
-              ()
-              break;
-            default:
+            default: quit();
               // code block
           }
-        //
+        
       });
 }
-function viewByManager(){
-    //map, key value pair ... associate key =unique data value= what the user will see (first + last)
-    // connection.query("SELECT first_name, last_name, manager_id FROM employee", function(err, res) {
-    //     if (err) throw err;
-    //     // Log all results of the SELECT statement
-    //     console.table(res);
-    //     connection.end();
-    //   });
-    //use inquirer to ask which manager id
-    //lookUpManger(response.choice)
-}
-// lookUpManger(1);
-
-function lookUpManger(id){
-    connection.query("SELECT * FROM employee where manager_id="+id+"", function(err, res) {
-        if (err) throw err;
-        // Log all results of the SELECT statement
-        console.table(res);
-        connection.end();
-      });
-
-}
-
 
 function addDepartment(){
 
 }
 
 function addRoles(){
-    
+    console.log("Updating all roles...\n");
+    let query = connection.query(
+      "UPDATE role SET ? WHERE ?",
+      [
+        {
+          title: ""
+        },
+        {
+          salary: "Rocky Road"
+        },
+        {
+            department_id: ""
+        }
+      ],
+      function(err, res) {
+        if (err) throw err;
+        console.log(res + " roles updated!\n");
+        // Call deleteProduct AFTER the UPDATE completes
+        deleteProduct();
+      }
+    );
+  
+    // logs the actual query being run
+    console.log(query.sql);
 }
 
 function addManager(){
@@ -182,6 +158,7 @@ function addEmployees(){
 }
 
 function viewRoles(){
+  console.log("Selecting all Rows...\n")
 connection.query("SELECT * FROM role", function(err, res) {
           if (err) throw err;
           // Log all results of the SELECT statement
@@ -192,10 +169,9 @@ connection.query("SELECT * FROM role", function(err, res) {
 }
 
 function viewDepartment(){
-    // console.log("Selecting all Employees...\n");
+     console.log("Selecting all Employees...\n");
         connection.query("SELECT * FROM department", function(err, res) {
           if (err) throw err;
-          // Log all results of the SELECT statement
           console.table(res);
           connection.end();
           mainMenu()
@@ -203,15 +179,13 @@ function viewDepartment(){
 }
 
 function viewEmployees(){
-        console.log("Selecting all Employees...");
+        console.log("Selecting all Employees...\n");
         connection.query("SELECT * FROM employee", function(err, res) {
           if (err) throw err;
-          // Log all results of the SELECT statement
           console.table(res);
           connection.end();
           mainMenu()
         });
-    
 }
 
 
