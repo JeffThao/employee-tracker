24244a169d418e209fdf7daf543e4350bc364986
Jeff T <jeff.thao92@gmail.com>
2020-10-27 18:13:12 -0500

    updated addRoles(), can now add new roles and salaries for that role.

--- index.js
+++ index.js
@@ -4,188 +4,194 @@ const mysql = require("mysql");
 const cTable = require('console.table');
 
 var connection = mysql.createConnection({
-    host: "localhost",
-  
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "root",
-    database: "employees_db"
-  });
-  
-  connection.connect(function(err) {
-    if (err) throw err;
-    mainMenu();
-  });
+  host: "localhost",
 
-function mainMenu(){
-    inquirer.prompt([
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "root",
+  database: "employees_db"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  mainMenu();
+});
+
+function mainMenu() {
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "choice",
+      message: "What would you like to do?",
+      choices: [
         {
-          type: "list",
-          name: "choice",
-          message: "What would you like to do?",
-          choices: [
-            {
-              name: "View All Employees",
-              value: "viewEmployees"
-            },
-            {
-              name: "View All Roles",
-              value: "viewAllRoles"
-            },
-            {
-                name: "View All Departments",
-                value: "viewDept"
-              },
-            {
-              name: "View Employees By Department",
-              value: "viewEmployeesByDept"
-            },
-            {
-              name: "Update Employee Role",
-              value: "updateEmployeeRole"
-            },
-            {
-              name: "Add an Employee",
-              value: "addEmployee"
-            },
-            {
-              name: "Add a Role",
-              value: "addRole"
-            },
-            {
-              name: "Add Department",
-              value: "addDept"
-            },
-            {
-              name: "Quit",
-              value: "quit"
-            }
-          ]
+          name: "View All Employees",
+          value: "viewEmployees"
+        },
+        {
+          name: "View All Roles",
+          value: "viewAllRoles"
+        },
+        {
+          name: "View All Departments",
+          value: "viewDept"
+        },
+        {
+          name: "View Employees By Department",
+          value: "viewEmployeesByDept"
+        },
+        {
+          name: "Update Employee Role",
+          value: "updateEmployeeRole"
+        },
+        {
+          name: "Add an Employee",
+          value: "addEmployee"
+        },
+        {
+          name: "Add a Role",
+          value: "addRole"
+        },
+        {
+          name: "Add Department",
+          value: "addDept"
+        },
+        {
+          name: "Quit",
+          value: "quit"
         }
-      ])
-      .then(function(response){
-          console.log(response)
+      ]
+    }
+  ])
+    .then(function (response) {
+      console.log(response)
 
-        //user data, what do I do with it?
-        switch(response.choice) {
-            case "viewEmployees":
-                viewEmployees();
-              break;
-              case "viewAllRoles":
-              // code block
-              console.log("works")
-              viewRoles()
-              break;
-              case "viewDept":
-              // code block
-              console.log("works")
-            //   Not sure if this function is correct viewDepartment()
-              break;
-            case "viewEmployeesByDept":
-              // code block
-              console.log("works")
-              viewDepartment()
-              break;
-              case "updateEmployeeRole":
-              // code block
-              console.log("works")
-              addRoles()
-              break;
-              case "addEmployee":
-              // code block
-              console.log("works")
-              addEmployees()
-              break;
-              case "addRole":
-              // code block
-              console.log("works")
-              addRoles()
-              break;
-              case "addDept":
-              // code block
-              console.log("works")
-              addDepartment()
-              break;
-            default: quit();
-              // code block
-          }
-        
-      });
+      //user data, what do I do with it?
+      switch (response.choice) {
+        case "viewEmployees":
+          viewEmployees();
+          break;
+        case "viewAllRoles":
+          // code block
+          console.log("works")
+          viewRoles()
+          break;
+        case "viewDept":
+          // code block
+          console.log("works")
+          //   Not sure if this function is correct viewDepartment()
+          break;
+        case "viewEmployeesByDept":
+          // code block
+          console.log("works")
+          viewDepartment()
+          break;
+        case "updateEmployeeRole":
+          // code block
+          console.log("works")
+          addRoles()
+          break;
+        case "addEmployee":
+          // code block
+          console.log("works")
+          addEmployees()
+          break;
+        case "addRole":
+          // code block
+          console.log("works")
+          addRoles()
+          break;
+        case "addDept":
+          // code block
+          console.log("works")
+          addDepartment()
+          break;
+        default: quit();
+        // code block
+      }
+
+    });
 }
 
-function addDepartment(){
+function addDepartment() {
 
 }
 
-function addRoles(){
+function addRoles() {
+  inquirer.prompt([
+      {
+      type: "input",
+      name: "role",
+      message: "What is the new Role?"
+    },
+    {
+      type: "input",
+      name: "salary",
+      message: "What is the salary for this role?"
+    }
+  ]).then(function(answer){
     console.log("Updating all roles...\n");
-    let query = connection.query(
+    connection.query(
       "UPDATE role SET ? WHERE ?",
       [
         {
-          title: ""
+          title: answer.role
         },
         {
-          salary: "Rocky Road"
-        },
-        {
-            department_id: ""
+          salary: answer.salary
         }
       ],
-      function(err, res) {
+      function (err, res) {
         if (err) throw err;
         console.log(res + " roles updated!\n");
-        // Call deleteProduct AFTER the UPDATE completes
-        deleteProduct();
       }
     );
-  
-    // logs the actual query being run
-    console.log(query.sql);
-}
-
-function addManager(){
+  })
 
 }
 
-function addEmployees(){
+function addManager() {
 
 }
 
-function viewRoles(){
+function addEmployees() {
+
+}
+
+function viewRoles() {
   console.log("Selecting all Rows...\n")
-connection.query("SELECT * FROM role", function(err, res) {
-          if (err) throw err;
-          // Log all results of the SELECT statement
-          console.table(res);
-          connection.end();
-          mainMenu()
-        });
+  connection.query("SELECT * FROM role", function (err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.table(res);
+    connection.end();
+    mainMenu()
+  });
 }
 
-function viewDepartment(){
-     console.log("Selecting all Employees...\n");
-        connection.query("SELECT * FROM department", function(err, res) {
-          if (err) throw err;
-          console.table(res);
-          connection.end();
-          mainMenu()
-        });
+function viewDepartment() {
+  console.log("Selecting all Employees...\n");
+  connection.query("SELECT * FROM department", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    connection.end();
+    mainMenu()
+  });
 }
 
-function viewEmployees(){
-        console.log("Selecting all Employees...\n");
-        connection.query("SELECT * FROM employee", function(err, res) {
-          if (err) throw err;
-          console.table(res);
-          connection.end();
-          mainMenu()
-        });
+function viewEmployees() {
+  console.log("Selecting all Employees...\n");
+  connection.query("SELECT * FROM employee", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    connection.end();
+    mainMenu()
+  });
 }
 
 
